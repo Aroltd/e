b84967b5ba5e9cdd4931600ad3beb7f772e3d351
Richard Shi <richardshi97@gmail.com>
2017-03-21 03:08:43 -0400

    Added hip new languages for E

---
+++ e.ex
@@ -0,0 +1,15 @@
+# Put in your mix.exs:
+
+#def project do
+#  [app: :E,
+#   version: "0.0.1",
+#   elixir: "~> 1.0",
+#   escript: [main_module: E], 
+#   deps: deps]
+#end
+
+defmodule E do
+  def main(args) do
+    IO.puts "e" 
+  end
+end
